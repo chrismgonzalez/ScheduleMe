1cfc087875476bce117d6c13113ca577bb44937c
Chris Gonzalez <cmgonza89@gmail.com>
2020-02-23 21:17:30 -0600

    changes to .gitignore

--- src/appointment/controllers/loginController.java
+++ src/appointment/controllers/loginController.java
@@ -5,38 +5,25 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+
 public class loginController {
 
-    @FXML private TextField userNameField;
-    @FXML private TextField passwordField;
-    @FXML private Label userNameLabel;
+    @FXML private TextField usernametxt;
+    @FXML private TextField passwordtxt;
+    @FXML private Label usernameLabel;
     @FXML private Label passwordLabel;
     @FXML private Button loginButton;
 
 
     @FXML
     public void initialize() {
-        loginButton.setDisable(true);
+        loginButton.setDisable(false);
     }
 
-
-    //disable login button if text fields are empty
     @FXML
-    public void handleKeyReleased() {
-        String userNameText = userNameField.getText();
-        String passwordText = passwordField.getText();
-
-        boolean disableLoginButton = (userNameText.isEmpty() || userNameText.trim().isEmpty()) || (passwordText.isEmpty() || passwordText.trim().isEmpty());
-
-        loginButton.setDisable(disableLoginButton);
-
-
-    }
-    @FXML
-    public void onLogin() {
+    public void handleLogin() {
         System.out.println("Logged In!");
     }
-
 
 }
 
